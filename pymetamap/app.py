8ee5006df340bb94cef9650a9b8fd917aef02b4d
nwams <ncn35@cornell.edu>
2015-11-12 14:33:17 -0500

    all ICD and CPT codes correctly being mapped to UMLS terms

--- pymetamap/app.py
+++ pymetamap/app.py
@@ -1,41 +1,57 @@
 from pymetamap import MetaMap
 import csv
 
+def getCPTandICD():
+    """
+    get CPT and ICD codes, put in dictionary
+    :return: the dictionary of codes
+    """
+    with open('../codes/CPTandICD.csv', 'rU') as csvfile:
+        reader = csv.reader(csvfile)
+        codeDict = {rows[0]:rows[1] for rows in reader}
+        return codeDict
 
 def setupStuff():
+    """
+    setup MetaMap and search UMLS for user desired topic
+    :return: The preferred name for the UMLS concept identified in the text. And the number of different preferred names output
+    """
     mm = MetaMap.get_instance('/Users/nwams/Documents/MetaMap/public_mm/bin/metamap12')
 
-    # make input lowercase?
-    searchTopic = raw_input('what topic are you searching for? ')
+    searchTopic = raw_input('What topic are you searching for? ')
+    print "the user is searching for:", searchTopic
     searchTopic = [searchTopic]
+
+    # [1] only allow user to search for one topic at a time
     concepts, error = mm.extract_concepts(searchTopic, [1])
 
     conceptArray = []
     for concept in concepts:
         conceptArray.append(concept.preferred_name)
-    print "conceptArray", conceptArray
+    print "UMLS terms = ", conceptArray
+
     return conceptArray, len(conceptArray)
 
-def getCPT():
-    with open('../codes/CPT.csv', 'rU') as csvfile:
-        reader = csv.reader(csvfile)
-        codeDict = {rows[0]:rows[1] for rows in reader}
-        print "Code Dictionary =", codeDict
-        return codeDict
 
-def searchCode(codeDict, searchTerms, lengthOfArray):
-    searchTerms[2] = 'Kidney' # delete later
-    watevs = []
-
-    """ find a way to append all key, value pair mappings
-    consider storing in dict, not tuples
+def searchThroughCodes(codeDict, searchTerms, numOfSearches):
     """
-    for i in range(lengthOfArray):
-        watevs = [(k, v) for (k, v) in codeDict.iteritems() if searchTerms[i] in v]
-    print watevs
+    search through the list of IPT/CPT codes.
+    if the users topic matches, via UMLS terms, assign it to a dictionary
+    :param codeDict: dictionary of all IPT/CPT codes
+    :param searchTerms: array of UMLS Concept preferred_names
+    :param numOfSearches: number of names in Concept the array
+    :return: matchingTopicsDict
+    """
+    matchingTopicsDict ={}
+    for i in range(numOfSearches):
+        for k,v in codeDict.iteritems():
+            if searchTerms[i].lower() in v.lower(): # case insensitive search required
+                matchingTopicsDict[k] = v
+    print matchingTopicsDict
+    return matchingTopicsDict
 
-codeDict = getCPT()
+codeDict = getCPTandICD()
 
-conceptArray, length = setupStuff()
+conceptArray, lengthOfConceptArray = setupStuff()
 
-searchCode(codeDict, conceptArray, length)
+searchThroughCodes(codeDict, conceptArray, lengthOfConceptArray)
